81d9457e77fe5aa3c08d0f09652b0e70fd68f2e0
Elvis Sánchez <84103320+Elvis102@users.noreply.github.com>
2026-05-16 09:53:03 -0500

    Implement file uploader for CSV files
    Add file uploader for CSV data in Streamlit app

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ import streamlit as st
 st.title("Proyecto UCG")
 st.sidebar.title("Parámetros")
 st.sidebar.image("Python_logo.png")
+
+uploaded_files = st.file_uploader(
+    "Upload data", accept_multiple_files=True, type="csv"
+)
+for uploaded_file in uploaded_files:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
